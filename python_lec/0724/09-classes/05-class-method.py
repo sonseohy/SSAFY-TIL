cd94aa817e08ba7b378123869f26da7c90857e46
bbdgs23 <sonseoh223@gmail.com>
2024-07-26 00:16:24 +0900

    0724_TIL 수정

--- python_lec/0724/09-classes/05-class-method.py
+++ python_lec/0724/09-classes/05-class-method.py
@@ -8,6 +8,7 @@ class Person:
     @classmethod
     def number_of_population(cls):
         print(f'인구수는 {cls.count}입니다.')
+        print(f'인구수는 {Person.count}입니다.') # 상속으로 인해 하위클래스가 생성될 수 있는데 하위 클래스가 number_of_population을 가져다 쓸 수 있는데 자식 클래스가 number_of_population을 제대로 못 씀
 
 
 person1 = Person('iu')
