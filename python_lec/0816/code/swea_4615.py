1b5403041d8977203c4c87f7dae8ed67e014a63a
bbdgs23 <sonseoh223@gmail.com>
2024-08-19 04:29:54 +0900

    0816_code+

--- python_lec/0816/code/swea_4615.py
+++ python_lec/0816/code/swea_4615.py
@@ -8,6 +8,9 @@ for test_case in range(1, T + 1):
 
     board = [[0] * N for _ in range(N)]
 
+    op = [0, 'W', 'B']  # 흑돌과 백돌의 인덱스에 반대색 저장
+    wb = [0, 'B', 'W']
+
     # 초기 setting
     for idx in range(2):
         board[N//2-idx][N//2-idx] = 'W'
@@ -16,19 +19,34 @@ for test_case in range(1, T + 1):
     di = [-1, 0, 1, 0, 1, 1, -1, -1]
     dj = [0, 1, 0, -1, 1, -1, 1, -1]
 
-    black = 0
-    white = 0
     for y, x, s in OTHELLO:
-        board[x - 1][y - 1] = s
+        board[x-1][y-1] = wb[s]     # 돌을 놓는 위치
         for k in range(8):
-            ni = x + di[k]
-            nj = y + dj[k]
-            if 0 <= ni < N and 0 <= nj < N:
-                if board[ni][nj] == 'W' and s == 1:
-                    board[ni][nj] = 'B'
-                    black += 1
-                elif board[ni][nj] == 'B' and s == 2:
-                    board[ni][nj] = 'W'
-                    white += 1
+            ni = (x-1) + di[k]
+            nj = (y-1) + dj[k]
+            tmp = []
+            while 0 <= ni < N and 0 <= nj < N and board[ni][nj] == op[s]:
+                tmp.append([ni, nj])
+                ni += di[k]
+                nj += dj[k]
 
-    print(f'#{test_case} {black} {white}')
+            if 0 <= ni < N and 0 <= nj < N and board[ni][nj] == wb[s]:
+                for ti, tj in tmp:
+                    board[ti][tj] = wb[s]
+
+    # for i in range(N):
+    #     print(*board[i])
+
+    b_cnt = 0
+    w_cnt = 0
+    for i in board:
+        for j in i:
+            if j == 'B':
+                b_cnt += 1
+            elif j == 'W':
+                w_cnt += 1
+    
+    print(f'#{test_case} {b_cnt} {w_cnt}')
+
+# 오답
+# 마지막 b, c 카우늩에서 if == 'B' 와 else로 쓰면 통과 못함
--- python_lec/0816/code/swea_5432.py
+++ python_lec/0816/code/swea_5432.py
@@ -2,27 +2,132 @@ T = int(input())
 
 for test_case in range(1, T+1):
     s = input()
-    # #
-    # r = 0
-    # idx = []
-    # for i in range(1, len(s)):
-    #     if s[i-1] == '(' and s[i] == ')':
-    #         r += 1
-    # stick = (len(s) - r * 2) // 2
 
-    stack = []
+    # 레이저의 위치를 찾아 저장할 stack 생성
+    laser_stack = []
     r = 0
     for i in range(len(s)):
-        if stack and stack[-1] == '(' and s[i] == ')':
-            stack.pop()
-            stack.append('o')
+        if laser_stack and laser_stack[-1] == '(' and s[i] == ')':
+            laser_stack.pop()
+            laser_stack.append('o')
             r += 1
         else:
-            stack.append(s[i])
-    print(*stack)
+            laser_stack.append(s[i])
+    print(*laser_stack)
+
+    stack = []
+    cnt = 0
+    result = 0
+    while laser_stack:
+        t = laser_stack.pop()
+        if t == ')':
+            stack.append(t)
+        elif stack and laser_stack and t == 'o':
+            tmp = 0
+            stack.append(t)
+            if laser_stack[-1] == '(':
+                cnt += 1
+                if stack and stack[-1] == 'o':
+                    while stack and stack[-1] == 'o':
+                        stack.pop()
+                        tmp += 1
+                        cnt += 1
+                result += cnt
+                stack.pop()
+                laser_stack.pop()
+                if stack:
+                    for _ in range(tmp):
+                        stack.append('o')
+                    cnt = 0
+            elif laser_stack[-1] == 'o':
+                continue
+
+        elif stack and t == '(':
+            cnt += 1
+            tmp = 0
+            if stack and stack[-1] == 'o':
+                while stack and stack[-1] == 'o':
+                    stack.pop()
+                    tmp += 1
+                    cnt += 1
+            result += cnt
+            if stack:
+                for _ in range(tmp):
+                    stack.append('o')
+            cnt = 0
+
+        if stack == []:
+            cnt = 0
+
+    print(f'#{test_case} {result}')
 
 
 
+# # 제한시간 초과 발생
+
+# T = int(input())
+
+# for test_case in range(1, T+1):
+#     s = input()
+
+#     # 레이저의 위치를 찾아 저장할 stack 생성
+#     laser_stack = []
+#     r = 0
+#     for i in range(len(s)):
+#         if laser_stack and laser_stack[-1] == '(' and s[i] == ')':
+#             laser_stack.pop()
+#             laser_stack.append('o')
+#             r += 1
+#         else:
+#             laser_stack.append(s[i])
+#     print(*laser_stack)
+
+#     stack = []
+#     cnt = 0
+#     result = 0
+#     while laser_stack:
+#         t = laser_stack.pop()
+#         if t == ')':
+#             stack.append(t)
+#         elif stack and laser_stack and t == 'o':
+#             tmp = 0
+#             stack.append(t)
+#             if laser_stack[-1] == '(':
+#                 cnt += 1
+#                 if stack and stack[-1] == 'o':
+#                     while stack and stack[-1] == 'o':
+#                         stack.pop()
+#                         tmp += 1
+#                         cnt += 1
+#                 result += cnt
+#                 stack.pop()
+#                 laser_stack.pop()
+#                 if stack:
+#                     for _ in range(tmp):
+#                         stack.append('o')
+#                     cnt = 0
+#             elif laser_stack[-1] == 'o':
+#                 continue
+#             # elif laser_stack[-1] == ')':
+#             #     cnt = 0
+#         elif stack and t == '(':
+#             cnt += 1
+#             tmp = 0
+#             if stack and stack[-1] == 'o':
+#                 while stack and stack[-1] == 'o':
+#                     stack.pop()
+#                     tmp += 1
+#                     cnt += 1
+#             result += cnt
+#             if stack:
+#                 for _ in range(tmp):
+#                     stack.append('o')
+#             cnt = 0
+
+#         if stack == []:
+#             cnt = 0
+
+#     print(f'#{test_case} {result}')
 # ((()())(())()))
 # (((1 2)(3) 4))
 #
