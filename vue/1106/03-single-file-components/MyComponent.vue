620949ec42d908d3d1244a21e438207722638c15
ssh <sonseoh223@gmail.com>
2024-11-07 10:48:48 +0900

    1107

---
+++ vue/1106/03-single-file-components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="greeting">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('hello')
+
+</script>
+
+<style scoped>
+.greeting {
+  color: red;
+}
+</style>
---
+++ vue/1107/04-component-state-flow/vue-project/src/components/ParentChild.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>{{ myMsg }}</p>
+    <p>{{ dynamicProps }}</p>
+    <ParentGrandChild 
+      :my-msg="myMsg"
+      @update-name="updateName"
+    />
+    <!-- <button @click="$emit('someEvent')">클릭</button> -->
+    <button @click="buttonClick">클릭</button>
+    <button @click="emitArgs">추가 인자 전달</button>
+  </div>
+</template>
+
+<script setup>
+import ParentGrandChild from '@/components/ParentGrandChild.vue';
+// 내려받은 props를 선언
+// 1. 배열 선언 방식
+// defineProps(['myMsg'])
+
+// 2. 객체 선언 방식
+defineProps({
+  myMsg: String,
+  dynamicProps: String
+})
+
+// props 데이터를 JavaScript에서 활용해야 하는 경우
+// const props = defineProps({ myMsg: String })
+// console.log(props)
+// console.log(props.myMsg)
+
+// emit 이벤트 선언 (선언방식 : 배열방식, 객체방식)
+const emit = defineEmits(['someEvent', 'emitArgs', 'updateName'])
+const buttonClick = function () {
+  emit('someEvent')
+}
+
+const emitArgs = function () {
+  emit('emitArgs', 1, 2, 3)
+}
+
+const updateName = function () {
+  emit('updateName')
+}
+
+</script>
+
+<style scoped>
+
+</style>
