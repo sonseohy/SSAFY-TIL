51c7cac5cbccaa8658dc5b04b5183a8eff4f179f
ssh <sonseoh223@gmail.com>
2024-11-12 18:01:02 +0900

    1112

---
+++ vue/1112/06-state-management/vue-project/src/components/TodoListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span
+      @click="store.updateTodo(todo.id)"
+      :class="{ 'is-done': todo.isDone }"
+    >
+      {{ todo.text }}
+    </span>
+    <button @click="store.deleteTodo(todo.id)">삭제</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+defineProps({
+  todo: Object
+})
+
+const store = useCounterStore()
+</script>
+
+<style sco0ped>
+.is-done {
+  text-decoration: line-through;
+}
+</style>
