3cabe5589ead9794982327997867d553d58c1731
ssh <sonseoh223@gmail.com>
2025-01-16 01:23:25 +0900

    update

---
+++ react/onebite/section01/chapter03.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+// JavaScript 실습 환경 설정하기
+// vscode extension
+// Live Server
+
+
+// index.html에서 Ctrl + Shift + P 누른 후 검색창에 live server를 검색해 Open with Live Server를 누르면 웹 페이지 확인 가능 (Live Server 플러그인이 제공하는 기능)
